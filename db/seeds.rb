193de86148a2a05589f044982d1dc78e7bd84b0e
Romain Coupey <schaft115@hotmail.com>
2020-05-26 19:02:37 +0200

    test commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,32 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'open-uri'
-require "nokogiri"
+# require 'open-uri'
+# require "nokogiri"
 
-# puts "let's seed the ingredients..."
+puts "let's seed the ingredients..."
 
-# Ingredient.delete_all
+Ingredient.delete_all
 
-# ingredients_list = JSON.parse(open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read)
+ingredients_list = JSON.parse(open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read)
 
-# ingredients_list['drinks'].each do |element|
-#   Ingredient.create!(name: element['strIngredient1'])
-# end
+ingredients_list['drinks'].each do |element|
+  Ingredient.create!(name: element['strIngredient1'])
+end
 
 puts "let's seed the cocktails..."
 
 cocktail_title = Nokogiri::HTML(open('https://imgur.com/gallery/O5nh9').read)
 cocktails_array = []
-cocktail_title.search('.post-image-title').each do |element|
-  cocktails_array << element.text.strip
+cocktail_title.search('.post-image-container').each do |element|
+  cocktails_array << {
+    name: element.search('.post-image-meta').search('.post-image-title').text.strip,
+    description: element.search('.post-image-meta').search('.post-image-description').text.strip,
+  }
 end
 
 Dose.delete_all
 Cocktail.delete_all
 
 cocktails_array.each do |cocktail|
-  puts Cocktail.create!(name: cocktail)
+  Cocktail.create!(
+    name: cocktail[:name],
+    description: cocktail[:description]
+  )
 end
 
 puts 'Done!'
